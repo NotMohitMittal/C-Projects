f43918cd3f7cf715883a4310c8d3aec815d091b4
Mohit Mittal <95767369+NotMohitMittal@users.noreply.github.com>
2024-02-26 03:31:25 +0530

    Banking Program using the switch

--- bankingProgram.cpp
+++ bankingProgram.cpp
@@ -1,84 +1,72 @@
 #include <iostream>
 #include <istream>
 #include <string>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
 using std::endl;
-using std::getline ;
+using std::getline;
 using std::string;
 
-void showBalance(int balance);
-double depositeMoney(double oldBalance, double addAmount);
-double withdrawMoney(double oldBalance, double subAmount);
+void showBalance(double balance);
+double depositeMoney(double oldBalance);
+double withdrawMoney(double oldBalance);
 void exitBank();
 
 int main()
 {
     int userPick;
     double userBalance = 45;
-    double deposite;
-    double withDraw;
-    double amount;
-
-    // Taking valid user Input
+    
     cout << "WELCOME TO THE BANIKING APP" << endl;
     cout << "---------------------------\n" << endl;
-        
     do
     {
         userPick = 0;
-        while (userPick != 1 && userPick != 2 && userPick != 3 && userPick != 4)
-        {
-            cout << "Choose your options:" << endl;
-            cout << "1. Show Balance" << endl;
-            cout << "2. Deposite Money" << endl;
-            cout << "3. Withdraw Money" << endl;
-            cout << "4. exit" << endl;
+        // Taking valid user Input
+        cout << "Choose your options:" << endl;
+        cout << "1. Show Balance" << endl;
+        cout << "2. Deposite Money" << endl;
+        cout << "3. Withdraw Money" << endl;
+        cout << "4. exit" << endl;
 
-            cout << "Choose ---> ";
-            cin >> userPick;
-        }
+        cout << "Choose ---> ";
+        cin >> userPick;
 
-        if (userPick == 1)
+        switch (userPick)
         {
-            showBalance(userBalance);
-        }
-        else if (userPick == 2)
-        {
-            cout << "Enter the Amount: ";
-            cin >> amount;
-            userBalance = depositeMoney(userBalance, amount);
-            cout << amount << " balance added sucessfully." << endl;
-            cout << "Current account balance " << userBalance << endl;
-        }
-        else if (userPick == 3)
-        {
-            cout << "Enter the Amount: ";
-            cin >> amount;
-            userBalance = withdrawMoney(userBalance, amount);
-            cout << amount << " balance withdraw sucessfully." << endl;
-            cout << "Current account balance " << userBalance << endl;
-        }
-        else if (userPick == 4)
-        {
-            exitBank();
+        case 1: showBalance(userBalance); break;
+        case 2: userBalance = depositeMoney(userBalance); break;
+        case 3: userBalance = withdrawMoney(userBalance); break;
+        case 4: exitBank(); break;
+        default:
+            cout << "Not a valid Input.\n" << endl;
+            break;
         }
     } while (userPick != 4);
-
+    
+   
     return 0;
 }
 
-void showBalance(int userBalance)
+void showBalance(double userBalance)
 {
-    cout << "Your Current Balance is " << userBalance << endl;
+    cout << "Your Current Balance is $" << std::setprecision(2) << std::fixed << userBalance << endl;
 }
-double depositeMoney(double oldBalance, double addAmount)
+double depositeMoney(double oldBalance)
 {
-    return oldBalance + addAmount;
+    double addAmount;
+    cout << "Enter the Amount: " ;
+    cin >> addAmount;
+    oldBalance = oldBalance + addAmount;
+    return oldBalance;
 }
-double withdrawMoney(double oldBalance, double subAmount)
+double withdrawMoney(double oldBalance)
 {
+    double subAmount;
+    cout << "Enter the Amount: " ;
+    cin >> subAmount;
     return oldBalance - subAmount;
 }
 void exitBank()
